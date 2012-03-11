09e3d869b73ccdff8b087c5f0f9f1f8cc75fe46b
lidongpeng1989@gmail.com <Damon@Damon-Lenovo.(none)>
2012-03-11 23:50:09 +0800

    add enum.js

---
+++ 9_ClassAndModule/js/enum.js
@@ -0,0 +1,31 @@
+﻿function enumeration(namesToValues) {
+    // ?? This is the dummy constructor that will be the return value.
+    var enumeration = function () { throw "Can't Instantiate Enumerations"; }
+
+    // Enumerated values inherit from this object
+    var proto = enumeration.prototype = {
+        constructor: enumeration,
+        toString: function () { return this.name; },
+        valueOf: function () { return this.value; },
+        toJSON: function () { return this.name; }
+    };
+
+    enumeration.values = []; // An array of the enumerated value objects
+
+    // Now create the instance of this new type
+    for (name in namesToValues) {
+        var e = inherit(proto);
+        e.name = name;
+        e.value = namesToValues[name];
+        enumeration[name] = e;
+        enumeration.values.push(e);
+    }
+
+    // a class method for iterating the instance of the class
+    enumeration.foreach = function (f, c) {
+        for (var i = 0; i < this.values.length; i++)
+            f.call(c, this.values[i]);
+    };
+    // Return the constructor that identifies the new type
+    return enumeration;
+}
